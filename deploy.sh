fbbb8b69b58e5fa4829f5b385264940ad3dd544c
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-07-21 13:17:29 +0900

    change path

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ rm -rf html
 git clone https://github.com/albatrosary/SampleAppDeploy.git html
 docker stop static_nginx
 docker rm static_nginx
-docker run -p 80:80 --name static_nginx -v /home/ashiras/Work/dockers/conf/static_nginx.conf:/etc/nginx/sites-enabled/default -v /home/ashiras/Work/dockers/html:/var/www -d test/nginx
+docker run -p 80:80 --name static_nginx -v `pwd`/conf/static_nginx.conf:/etc/nginx/sites-enabled/default -v `pwd`/html:/var/www -d test/nginx
